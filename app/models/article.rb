8f047f6a2721ef8fc8b8666d367acf160abd790e
Arif Hussain <ahussain@ezcater.com>
2015-11-01 20:35:21 -0500

    create new template, action and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-	validates :title, presence: true, { minimum: 3, maximum: 50 } 
-	validates :description, presence: true, { minimum: 10, maximum: 300 } 
+	validates :title, presence: true
+	validates :description, presence: true
 end 
